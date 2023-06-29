3013b6549bfee93ec562ca9848357fed6f0991fe
Lalithya <lalithya.paruchuri@gmail.com>
2023-06-28 22:57:08 -0400

    Implement toast feature and like button functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,65 @@
-import React from 'react';
-import Container from '@mui/material/Container';
+import React, { useState, useEffect } from "react";
+import Container from "@mui/material/Container";
 
-import Header from './Header';
-import Content from './Content';
+import Header from "./Header";
+import Content from "./Content";
+
+import { saveLikedFormSubmission, onMessage } from "./service/mockServer";
+
+// Number of times to fetch the data even when the server throws error
+const MAX_RETRIES = 3;
 
 function App() {
+  const [formData, setFormData] = useState("");
+  const [showToast, setShowToast] = useState(false);
+  const [disableLikeButton, setDisableLikeButton] = useState(false);
+  const [retry, setRetry] = useState({count: 0, error: false});
+
+  useEffect(() => {
+    if (retry.count < MAX_RETRIES && retry.error ) {
+      onClickLikeButton();
+    } else {
+      disableLikeButton && setDisableLikeButton(false);
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [retry]);
+
+  const onClickLikeButton = async (val) => {
+    try {
+      const data = { ...formData, data: { ...formData.data, liked: true } };
+      const response = await saveLikedFormSubmission(data);
+      if (response.status === 202) {
+        retry.error && setRetry({...retry, error: false});
+        setShowToast(false);
+      }
+    } catch (error) {
+      console.log(error);
+      setRetry({...retry, count: val ? 1 : retry.count + 1, error: true});
+    }
+  };
+
+  function handleFormSubmission(data) {
+    if (data) {
+      setFormData(data);
+      if (!showToast) {
+        setShowToast(true);
+        setDisableLikeButton && setDisableLikeButton(false);
+      }
+    }
+  }
+
+  onMessage(handleFormSubmission);
+
   return (
     <>
-      <Header />
+      <Header
+        data={formData}
+        showToast={showToast}
+        setShowToast={setShowToast}
+        onClickLikeButton={() => { setDisableLikeButton(true);  onClickLikeButton(true); }}
+        disableLikeButton={disableLikeButton}
+        setDisableLikeButton={setDisableLikeButton}
+      />
       <Container>
         <Content />
       </Container>
--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import Box from '@mui/material/Box';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
@@ -7,10 +6,68 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Snackbar from '@mui/material/Snackbar';
+import CloseIcon from '@mui/icons-material/Close';
+import MuiAlert from '@mui/material/Alert';
+import { styled } from "@mui/material/styles";
 
 import { createMockFormSubmission } from './service/mockServer';
 
-export default function Header() {
+const Alert = styled(MuiAlert)(() => ({
+  backgroundColor: '#000',
+  color: '#fff',
+
+  '> .MuiAlert-icon': {
+    display: 'none',
+  },
+
+  '> .MuiAlert-action': {
+    alignItems: 'center', 
+
+    '> button:first-of-type': {
+      color: '#33FFF0',
+      margin: '0 20px',
+    },
+
+    '> button:disabled': {
+      color: '#ddd',
+      margin: '0 20px',
+    }
+  },
+
+  '> .MuiAlert-message > p': {
+    margin: '5px 0',
+  }
+}));
+
+const NameContainer = styled('p')(() => ({
+  display: 'flex',
+  gap: '10px'
+}));
+
+export default function Header(props) {
+  const {data, ...rest} = props;
+
+  const handleToastClose = () => {
+    rest.setShowToast(false);
+  };
+
+  const action = (
+    <React.Fragment>
+      <Button color="secondary" size="small" onClick={rest.onClickLikeButton} disabled={rest.disableLikeButton}>
+        LIKE
+      </Button>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleToastClose}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </React.Fragment>
+  );
+
   return (
     <Box sx={{flexGrow: 1}}>
       <AppBar position="static">
@@ -24,8 +81,22 @@ export default function Header() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" sx={{flexGrow: 1}}>
-            Toast Exercise
-          </Typography>
+          <Snackbar
+            open={rest.showToast}
+            onClose={handleToastClose}
+            anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+          >
+            <Alert onClose={handleToastClose} sx={{ width: '100%' }} action={action}>
+              <NameContainer>
+                  <span>{data?.data?.firstName}</span>
+                  <span>{data?.data?.lastName}</span>
+              </NameContainer>
+              <p>
+                {data?.data?.email}
+              </p>
+            </Alert>
+          </Snackbar>
+          </Typography> 
           <Button
             variant="contained"
             size="small"
